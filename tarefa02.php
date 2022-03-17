8f2cb833267532858f7a80db1c12819888deba6d
otaviociacci <101129947+otaviociacci@users.noreply.github.com>
2022-03-17 14:36:38 -0300

    commit

---
+++ tarefa02.php
@@ -0,0 +1,18 @@
+<?php
+
+$valor01 = 2;
+$valor02 = 10;
+
+function soma($valor01, $valor02)  {
+    $return = $valor01 + $valor02;
+}
+
+function quadrado ($valor01) {
+    return pow ($valor01, 2);
+}
+
+echo soma($valor01, $valor02);
+echo '<br>';
+echo quadrado($valor01);
+
+?>
